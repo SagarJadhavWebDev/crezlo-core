6f63f27113f9f1bb99917e1d9177a76a701cf986
Sagar Jadhav <jadhavsagar.live@gmail.com>
2025-07-01 12:15:14 +0530

    w

--- src/home.tsx
+++ src/home.tsx
@@ -1,27 +1,38 @@
-'use client'
+'use client';
+
+import { useEffect, useState } from 'react';
+import './styles/style.css';
+
 export default function Home() {
+  const [cookieValue, setCookieValue] = useState<string | null>(null);
+
   function getCookie(name: string) {
-    console.log('getCookie1')
-    if (typeof window === "undefined") return null;
-    function escape(s: any) {
+    console.log('getCookie1');
+    if (typeof window === 'undefined') return null;
+    function escape(s: string) {
       return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, "\\$1");
     }
-    var match = document.cookie.match(
+    const match = document.cookie.match(
       RegExp("(?:^|;\\s*)" + escape(name) + "=([^;]*)")
     );
-    console.log('getCookie2')
+    console.log('getCookie2');
     return match ? match[1] : null;
   }
+
   const handleLocalStorage = () => {
-    const data = getCookie("auth");
-    console.log("from pacakge", data);
-    localStorage.setItem("test", "1msadncmsd23");
+    const data = getCookie('auth');
+    console.log('from package', data);
+    localStorage.setItem('test', '1msadncmsd23');
   };
+
+  useEffect(() => {
+    setCookieValue(getCookie('auth'));
+  }, []);
 
   return (
     <div onClick={handleLocalStorage}>
       <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world! + {getCookie("auth")}
+        Hello world! + {cookieValue}
       </h1>
     </div>
   );
