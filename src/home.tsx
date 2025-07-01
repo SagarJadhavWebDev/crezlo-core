65bffc815a0f5df103c67e8627f89f49d26c3662
Sagar Jadhav <jadhavsagar.live@gmail.com>
2025-07-01 12:05:26 +0530

    p

--- src/home.tsx
+++ src/home.tsx
@@ -1,11 +1,6 @@
 export default function Home() {
-  const handleLocalStorage = () => {
-    const data = getCookie("auth");
-    console.log("from pacakge", data);
-    localStorage.setItem("test", "1msadncmsd23");
-  };
-
   function getCookie(name: string) {
+    console.log('getCookie1')
     if (typeof window === "undefined") return null;
     function escape(s: any) {
       return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, "\\$1");
@@ -13,8 +8,14 @@ export default function Home() {
     var match = document.cookie.match(
       RegExp("(?:^|;\\s*)" + escape(name) + "=([^;]*)")
     );
+    console.log('getCookie2')
     return match ? match[1] : null;
   }
+  const handleLocalStorage = () => {
+    const data = getCookie("auth");
+    console.log("from pacakge", data);
+    localStorage.setItem("test", "1msadncmsd23");
+  };
 
   return (
     <div onClick={handleLocalStorage}>
