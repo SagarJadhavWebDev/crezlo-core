3b214bd9a5b6e9b7d6a940dab559a69ae5162b49
Sagar Jadhav <jadhavsagar.live@gmail.com>
2025-07-01 12:19:09 +0530

    w

--- src/home.tsx
+++ src/home.tsx
@@ -1,32 +1,32 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import './styles/style.css';
+import { useEffect, useState } from "react";
+import "./styles/style.css";
 
 export default function Home() {
   const [cookieValue, setCookieValue] = useState<string | null>(null);
 
-  function getCookie(name: string) {
-    console.log('getCookie1');
-    if (typeof window === 'undefined') return null;
+  const getCookie = (name: string) => {
+    console.log("getCookie1");
+    if (typeof window === "undefined") return null;
     function escape(s: string) {
       return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, "\\$1");
     }
     const match = document.cookie.match(
       RegExp("(?:^|;\\s*)" + escape(name) + "=([^;]*)")
     );
-    console.log('getCookie2');
+    console.log("getCookie2");
     return match ? match[1] : null;
-  }
+  };
 
   const handleLocalStorage = () => {
-    const data = getCookie('auth');
-    console.log('from package', data);
-    localStorage.setItem('test', '1msadncmsd23');
+    const data = getCookie("auth");
+    console.log("from package", data);
+    localStorage.setItem("test", "1msadncmsd23");
   };
 
   useEffect(() => {
-    setCookieValue(getCookie('auth'));
+    setCookieValue(getCookie("auth"));
   }, []);
 
   return (
